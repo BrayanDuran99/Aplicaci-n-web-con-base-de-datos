f198f045cabee9203492ef84e96db27e047ea911
Brayan Durán <123616961+BrayanDuran99@users.noreply.github.com>
2025-07-07 01:26:37 -0600

    app.py

--- estudiantes-app/app.py
+++ estudiantes-app/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
 
@@ -58,4 +59,4 @@ def delete(id):
 
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
